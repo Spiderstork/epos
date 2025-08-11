752956489d0af846f741ac20b4472ea022514bd7
ewan <ewan.peckham2008@gmail.com>
2025-08-11 21:53:49 +0100

    just made it so auto install quasar

--- start.bat
+++ start.bat
@@ -4,5 +4,10 @@ start "Node Server" cmd /k "node server.js"
 popd
 
 pushd "C:\epos\quasar-project"
+REM Install Quasar CLI and dependencies if not present
+if not exist node_modules (
+  call npm install -g @quasar/cli
+  call npm install
+)
 start "Quasar Dev" cmd /k "quasar dev"
 popd
